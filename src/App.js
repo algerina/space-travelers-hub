85bd2a670733ef7005428643c800259060d59c23
Amel Khiri <53347966+algerina@users.noreply.github.com>
2021-12-21 20:18:28 +0100

    Create a Route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
+import React from 'react';
+import {
+  BrowserRouter as Router, Routes,
+  Route,
+  NavLink,
+} from 'react-router-dom';
+import planet from './images/planet.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <div>
+            <img src={planet} alt="planet" />
+            <h1 className="bg-dark-red">Space Travelers Hub</h1>
+            <ul>
+              <li><NavLink href="/rockets" activeclassname="active">Rockets</NavLink></li>
+              <li><NavLink href="/missions" activeclassname="active">Missions</NavLink></li>
+              <li>|</li>
+              <li><NavLink href="/myProfile" activeclassname="active">My Profile</NavLink></li>
+            </ul>
+          </div>
+        </nav>
+        <div>
+          <Routes>
+            <Route exact path="/" />
+            <Route exact path="/" />
+            <Route exact path="/" />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Nav from './components/Nav';
+import App from './App';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Nav />
+      <App />
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
