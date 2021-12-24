ce94c9697e0170eb713f623de2815831ec875afe
Amel Khiri <53347966+algerina@users.noreply.github.com>
2021-12-24 16:40:25 +0100

    Fix linter issues

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,16 +1,24 @@
-import { useDispatch } from 'react-redux';
+/* eslint-disable camelcase */
+import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { bookRocket } from '../Redux/rockets/rockets';
+import { bookRocket, cancelReservation } from '../Redux/rockets/rockets';
 
 const Rocket = (props) => {
   const dispatch = useDispatch();
+
   const {
     id, rocket_name, description, rocket_image, reserved,
   } = props;
 
-  const bookingHandler = (id) => {
-    dispatch(bookRocket(id));
+  const rocketStatus = useSelector((state) => state.rocketReducer).filter((e) => e.id === id);
+
+  const bookingHandler = (rocket) => {
+    dispatch(bookRocket(rocket));
+  };
+
+  const cancelReservationgHandler = (rocket) => {
+    dispatch(cancelReservation(rocket));
   };
 
   return (
@@ -23,7 +31,23 @@ const Rocket = (props) => {
           {reserved ? <span className="reserved-button">Reserved</span> : ''}
           {description}
         </p>
-        {reserved ? <button type="button" onClick={() => bookingHandler(id)} className="cancel-button">Cancel Reservation</button> : <button type="button" onClick={() => reservationHandler(id)} className="reserve-button">Reserve Rocket</button>}
+        {reserved ? (
+          <button
+            type="button"
+            onClick={() => bookingHandler(id)}
+            className="cancel-button"
+          >
+            Cancel Reservation
+          </button>
+        ) : (
+          <button
+            type="button"
+            onClick={() => reservationHandler(id)}
+            className="reserve-button"
+          >
+            Reserve Rocket
+          </button>
+        )}
       </div>
     </div>
   );
@@ -32,11 +56,10 @@ const Rocket = (props) => {
 //   const rocketDisplay = useSelector((state) => state.rocketReducer).filter((e) => e.id === id);
 // };
 
-
 Rocket.propTypes = {
   id: PropTypes.string.isRequired,
   rocket_name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  rocket_image: proptypes.string.isRequired,
+  rocket_image: Proptypes.string.isRequired,
 };
 export default Rocket;
